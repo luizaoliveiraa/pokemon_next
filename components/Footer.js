c67f110384d4891c1c211a7d582096c2d6dde4ed
Luiza <luizadeoliveira@outlook.com>
2024-07-22 15:42:42 +0100

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,23 @@
+import Link from "next/link";
 import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>
-        <span>Luiza Oliveira</span> &copy; 2024
-      </p>
+      <div >
+        <span>All rights reserved - Luiza Oliveira</span> &copy; 2024
+      </div>
+      <div className={styles.socials}>
+        <Link href="https://github.com/luizaoliveiraa" target="_blank">
+          Github
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/luizaoliveira0915/"
+          target="_blank"
+        >
+          LinkedIn
+        </Link>
+      </div>
     </footer>
   );
 }
